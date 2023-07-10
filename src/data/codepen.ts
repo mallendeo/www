58510bfce87d314634407735be36367ea5e4a004
Mauricio Allende <github@mallendeo.com>
2023-07-10 12:31:18 -0400

    add codepen and dribbble data

--- src/data/codepen.ts
+++ src/data/codepen.ts
@@ -1,17 +1,78 @@
 export interface CodepenItem {
 	type: 'codepen'
 	url: string
-	title: string
 	slug: string
+	slugCdp: string
+	title: string
 	year: number
+	video?: string
+	border?: boolean
 }
 
 export const codepenItems: CodepenItem[] = [
 	{
-		type: 'codepen',
-		url: 'asd',
-		slug: 'asd',
-		title: 'asd',
+		slug: 'nvidia-outro',
+		slugCdp: 'JmNGem',
+		title: 'nvidia Outro',
+		video: 'nvidia.mp4',
 		year: 2018,
 	},
-]
+	{
+		slug: 'dave2d-intro',
+		slugCdp: 'qywwMB',
+		border: true,
+		title: 'Dave2D YouTube Intro',
+		video: 'dave2d.mp4',
+		year: 2018,
+	},
+	{
+		slug: 'ltt-old-intro',
+		slugCdp: 'bYERav',
+		title: 'Linus Tech Tips Old Intro',
+		video: 'ltt.mp4',
+		year: 2017,
+	},
+	{
+		slug: 'mkbhd-intro',
+		slugCdp: 'dvbQQx',
+		title: 'Marques Brownlee (aka MKBHD) Intro',
+		video: 'mkbhd.mp4',
+		year: 2018,
+	},
+	{
+		slug: 'pango-intro',
+		slugCdp: 'xORqVp',
+		title: 'Pango Intro',
+		video: 'pango.mp4',
+		year: 2016,
+	},
+	{
+		slug: 'gamecube-intro',
+		slugCdp: 'xORqVp',
+		title: 'Gamecube (Pure CSS)',
+		video: 'gamecube.mp4',
+		year: 2014,
+	},
+	{
+		slug: 'scoreboard',
+		slugCdp: 'EowXrm',
+		title: 'Scoreboard (Pure CSS)',
+		video: 'scoreboard.mp4',
+		year: 2018,
+	},
+	{
+		slug: 'cardano',
+		slugCdp: 'qBRNQPv',
+		title: 'Cardano Logo',
+		video: 'cardano.mp4',
+		year: 2021,
+	},
+].map(
+	(item) =>
+		({
+			...item,
+			type: 'codepen',
+			url: `https://codepen.io/mallendeo/pen/${item.slugCdp}`,
+			video: item.video ? `/videos/codepen/${item.video}` : undefined,
+		}) satisfies CodepenItem,
+)
--- src/data/dribbble.ts
+++ src/data/dribbble.ts
@@ -1,49 +1,53 @@
 export interface DribbbleItem {
-	type: 'dribbble'
+	type?: 'dribbble'
 	url: string
 	img?: string
 	title: string
 	year: number
+	border?: boolean
+	className?: string
 }
 
-export const dribbbleItems = [
+export const items: DribbbleItem[] = [
 	{
-		type: 'dribbble',
 		url: 'https://dribbble.com/shots/5167234-Icons-App',
 		title: 'Icons App Mockup',
 		year: 2018,
 	},
 	{
-		type: 'dribbble',
 		url: 'https://dribbble.com/shots/5276443-Menu',
 		title: 'Minimal Brutalist Menu',
 		year: 2018,
+		border: true,
 	},
 	{
-		type: 'dribbble',
 		url: 'https://dribbble.com/shots/5498050-Home',
 		title: 'Real Estate App Logo',
 		year: 2018,
 	},
 	{
-		type: 'dribbble',
 		url: 'https://dribbble.com/shots/5519867-Menu',
 		title: 'Real Estate App Mockup',
 		year: 2018,
 	},
 	{
-		type: 'dribbble',
 		url: 'https://dribbble.com/shots/5476158-Coffee',
 		title: 'Coffee',
 		year: 2018,
 	},
 	{
-		type: 'dribbble',
 		url: 'https://dribbble.com/shots/15417744-Kiwi-App-Icon',
 		title: 'Kiwi App Icon',
 		year: 2021,
 	},
-].map((item) => ({
-	...item,
-	img: `/dribbble/${item.title}.png`.toLowerCase().split(/\s+/).join('-'),
-})) as DribbbleItem[]
+]
+
+export const dribbbleItems: DribbbleItem[] = items.map(
+	(item) =>
+		({
+			...item,
+			type: 'dribbble',
+			className: item.className ?? 'text-white',
+			img: `/dribbble/${item.title}.png`.toLowerCase().split(/\s+/).join('-'),
+		}) satisfies DribbbleItem,
+)
