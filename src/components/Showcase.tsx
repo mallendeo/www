c48080ff37b97e20d6ce5785aeab6df109831331
Mauricio Allende <github@mallendeo.com>
2023-12-05 19:41:40 -0300

    reorganize

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -8,7 +8,7 @@ import { codepenItems } from '../data/codepen'
 import ShowcaseGrid from './ShowcaseGrid'
 
 const Title: ParentComponent = (props) => (
-	<h1 class='font-bold text-2xl text-gray-7 flex items-center gap-2'>
+	<h1 class="font-bold text-2xl text-gray-7 flex items-center gap-2">
 		{props.children}
 	</h1>
 )
@@ -16,7 +16,83 @@ const Title: ParentComponent = (props) => (
 const Showcase: Component<{ class?: string }> = (props) => (
 	<div class={clsx('flex flex-col gap-3', props.class)}>
 		<Title>
-			<BiLogosDribbble fill='#D13880' /> Dribbble
+			<BiLogosCodepen fill="currentColor" /> CodePen
+		</Title>
+
+		<ShowcaseGrid
+			items={codepenItems.map((item) => ({
+				content: (props) =>
+					item.video ? (
+						(() => {
+							let videoRef: HTMLVideoElement | undefined
+
+							createEffect(() => {
+								if (!videoRef) return
+								if (props.isVisible) {
+									videoRef.currentTime = 0
+									videoRef.loop = true
+									videoRef.play()
+									return
+								}
+								videoRef.pause()
+								videoRef.currentTime = videoRef.duration || 0
+							})
+
+							return (
+								<video
+									ref={(el) => {
+										videoRef = el
+									}}
+									muted
+									playsinline
+									src={item.video}
+								/>
+							)
+						})()
+					) : (
+						<iframe
+							class="pointer-events-none scale-50 origin-tl w-[200%] h-[200%] border-none absolute top-0 left-0"
+							src={item.url}
+							title={item.title}
+							sandbox="allow-scripts allow-pointer-lock allow-same-origin"
+							tabindex="-1"
+						/>
+					),
+				title: item.title,
+				border: item.border,
+				url: item.url,
+				iconClass: (props) =>
+					clsx(
+						'bg-zinc-900 group-hover:(shadow-zinc-900) group-active:(shadow-zinc-900)',
+						props.isVisible && 'shadow-zinc-900',
+					),
+				icon: BiLogosCodepen,
+			}))}
+		/>
+
+		<a
+			class={clsx(
+				'flex justify-center items-center w-full py-3',
+				'rounded-lg bg-gray-1 text-gray-5 transition-all',
+				'max-w-sm mx-auto mt-4',
+				'group hover:(bg-black text-white)',
+			)}
+			href="https://codepen.io/mallendeo"
+			target="__blank"
+			rel="noopener"
+		>
+			See more on my{' '}
+			<BiLogosCodepen
+				class="ml-2 mr-0.5 group-hover:(text-white) text-black transition-all"
+				fill="currentColor"
+			/>{' '}
+			CodePen profile!
+		</a>
+
+		<div class="h-0 my-10 mx-auto w-1/4 border-b-gray-200 border-b" />
+
+		<Title>
+			<BiLogosDribbble fill="#D13880" /> Dribbble
 		</Title>
 
 		<ShowcaseGrid
@@ -54,92 +130,16 @@ const Showcase: Component<{ class?: string }> = (props) => (
 				'max-w-sm mx-auto mt-4',
 				'group hover:(bg-rose text-white)',
 			)}
-			href='https://dribbble.com/mallendeo'
-			target='__blank'
-			rel='noopener'
+			href="https://dribbble.com/mallendeo"
+			target="__blank"
+			rel="noopener"
 		>
 			See more on my{' '}
 			<BiLogosDribbble
-				class='ml-2 mr-0.5 group-hover:(text-white) text-rose transition-all'
-				fill='currentColor'
+				class="ml-2 mr-0.5 group-hover:(text-white) text-rose transition-all"
+				fill="currentColor"
 			/>{' '}
 			Dribbble profile!
-		</a>
-
-		<div class='h-0 my-10 mx-auto w-1/4 border-b-gray-200 border-b' />
-
-		<Title>
-			<BiLogosCodepen fill='currentColor' /> CodePen
-		</Title>
-
-		<ShowcaseGrid
-			items={codepenItems.map((item) => ({
-				content: (props) =>
-					item.video ? (
-						(() => {
-							let videoRef: HTMLVideoElement | undefined
-
-							createEffect(() => {
-								if (!videoRef) return
-								if (props.isVisible) {
-									videoRef.currentTime = 0
-									videoRef.loop = true
-									videoRef.play()
-									return
-								}
-								videoRef.pause()
-								videoRef.currentTime = videoRef.duration || 0
-							})
-
-							return (
-								<video
-									ref={(el) => {
-										videoRef = el
-									}}
-									muted
-									playsinline
-									src={item.video}
-								/>
-							)
-						})()
-					) : (
-						<iframe
-							class='pointer-events-none scale-50 origin-tl w-[200%] h-[200%] border-none absolute top-0 left-0'
-							src={item.url}
-							title={item.title}
-							sandbox='allow-scripts allow-pointer-lock allow-same-origin'
-							tabindex='-1'
-						/>
-					),
-				title: item.title,
-				border: item.border,
-				url: item.url,
-				iconClass: (props) =>
-					clsx(
-						'bg-zinc-900 group-hover:(shadow-zinc-900) group-active:(shadow-zinc-900)',
-						props.isVisible && 'shadow-zinc-900',
-					),
-				icon: BiLogosCodepen,
-			}))}
-		/>
-
-		<a
-			class={clsx(
-				'flex justify-center items-center w-full py-3',
-				'rounded-lg bg-gray-1 text-gray-5 transition-all',
-				'max-w-sm mx-auto mt-4',
-				'group hover:(bg-black text-white)',
-			)}
-			href='https://codepen.io/mallendeo'
-			target='__blank'
-			rel='noopener'
-		>
-			See more on my{' '}
-			<BiLogosCodepen
-				class='ml-2 mr-0.5 group-hover:(text-white) text-black transition-all'
-				fill='currentColor'
-			/>{' '}
-			CodePen profile!
 		</a>
 	</div>
 )
